1e77d8dfd6e25d550bea33e93bc0247e182d76b7
AAstahckin <ALVAstashkin91@yandex.ru>
2023-07-04 12:06:28 +0300

    fix

--- src/test/java/api/service/Requests.java
+++ src/test/java/api/service/Requests.java
@@ -1,8 +1,8 @@
 package api.service;
 
-import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.ResponseSpecification;
+import lombok.val;
 
 import static api.specs.Specs.requestSpec;
 import static api.specs.Specs.response204Spec;
@@ -12,13 +12,13 @@ import static io.restassured.RestAssured.given;
 public class Requests {
 
     public static <T> T sendGetRequestGetUser(String endpoint, Class<T> responseClass, ResponseSpecification responseSpecification) {
-        Response response = step("Выполняется вызов метода GET, на " + endpoint, () -> given(requestSpec)
+        val response = step("Выполняется вызов метода GET, на " + endpoint, () -> given(requestSpec)
                 .when().get(endpoint).then().spec(responseSpecification).extract().response());
         return response.as(responseClass);
     }
 
     public static <T> T sendGetRequest(String endpoint, String queryParam, int queryParamValue, Class<T> responseClass, ResponseSpecification responseSpecification) {
-        Response response = step("Выполняется вызов метода GET, на " + endpoint, () -> given(requestSpec)
+        val response = step("Выполняется вызов метода GET, на " + endpoint, () -> given(requestSpec)
                 .queryParam(queryParam, queryParamValue)
                 .when().get(endpoint).then().spec(responseSpecification).extract().response());
         return response.as(responseClass);
@@ -26,14 +26,14 @@ public class Requests {
 
 
     public static <T> T sendPostRequest(String endpoint, Object body, Class<T> responseClass, ResponseSpecification responseSpecification) {
-        Response response = step("Выполняется вызов метода POST, на " + endpoint, () -> given(requestSpec)
+        val response = step("Выполняется вызов метода POST, на " + endpoint, () -> given(requestSpec)
                 .body(body)
                 .when().post(endpoint).then().spec(responseSpecification).extract().response());
         return response.as(responseClass);
     }
 
     public static <T> T sendPutRequest(String endpoint, Object body, Class<T> responseClass, ResponseSpecification responseSpecification) {
-        Response response = step("Выполняется вызов метода PUT, на " + endpoint, () -> given(requestSpec)
+        val response = step("Выполняется вызов метода PUT, на " + endpoint, () -> given(requestSpec)
                 .body(body)
                 .when().put(endpoint).then().spec(responseSpecification).extract().response());
         return response.as(responseClass);
